97f97850db919a8b48c2d5af829ca523f3ff4817
sandydeshm <65502160+sandydeshm@users.noreply.github.com>
2026-03-20 15:52:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -12,14 +13,14 @@ def home():
 @app.route('/recommend', methods=['POST'])
 def recommend():
     budget = int(request.form['budget'])
-    usage = request.form['usage']
+    usage = request.form['usage'].lower()
+
+    data['usage'] = data['usage'].str.lower()
 
     result = data[(data['price'] <= budget) & (data['usage'] == usage)]
 
     return render_template('result.html', laptops=result.to_dict(orient='records'))
 
 if __name__ == '__main__':
-import os
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
