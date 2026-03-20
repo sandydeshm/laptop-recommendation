78bb8068383728f948f94e222632fedcead5a3df
sandydeshm <65502160+sandydeshm@users.noreply.github.com>
2026-03-20 14:50:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,7 @@ def recommend():
     return render_template('result.html', laptops=result.to_dict(orient='records'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
